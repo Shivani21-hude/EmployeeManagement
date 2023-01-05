3991e529a3c2a95f0972fb3ba54e044a127c8ab7
Shivani21-hude <shivani21hude@gmail.com>
2023-01-05 17:52:12 +0530

    [Shivani]-Check employee is present or absent

---
+++ Employee_Management/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management
+{
+    public class Employee
+    {
+        public static void UC1Attendence()
+        {
+
+            int IsPresent = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IsPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
---
+++ Employee_Management/Program.cs
@@ -0,0 +1,16 @@
+﻿using Employee_Management;
+using System;
+namespace EmployeeManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Management \n");
+
+            Employee.UC1Attendence();
+
+
+        }
+    }
+}
